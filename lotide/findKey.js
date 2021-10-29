008399beb29ed80ff7b42916e39f26027b110ef9
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-10-29 03:25:35 +0000

    findKey.js

---
+++ lotide/findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const findKey = (object,value) => {
+  for (let eachKey in object) {
+    if (value(object[eachKey])) {
+      return eachKey;
+    }
+  }
+};
+
+const objects = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(objects,x => x.stars === 2),"noma");
