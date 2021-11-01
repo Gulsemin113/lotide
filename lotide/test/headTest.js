681c2dbebbb470ba3bc4f70f13aa3a58720ba98b
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-11-01 22:24:56 +0000

    Exporting All Functions II

--- lotide/test/headTest.js
+++ lotide/test/headTest.js
@@ -2,6 +2,7 @@
 const head = require ('../head');
 const assert = require('chai').assert;
 
+
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
